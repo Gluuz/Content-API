5ab6e9a247e2487e1d2791ee5023673b2a9f7477
Bruno Gluszczuk <brunogluszczukk1@gmail.com>
2022-01-10 16:36:14 -0300

    Creating first Endpoint 'AddMovie'

---
+++ ContentAPI/Controllers/MovieController.cs
@@ -0,0 +1,23 @@
+﻿using ContentAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContentAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : Controller
+    {
+        public static List<MovieModel> movies = new List<MovieModel>();
+
+        [HttpPost]
+        public void AddMovies([FromBody]MovieModel movie)
+        {
+            movies.Add(movie);
+            Console.WriteLine(movie.Title);
+        }
+    }
+}
---
+++ ContentAPI/Models/MovieModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContentAPI.Models
+{
+    public class MovieModel
+    {
+        public string Title { get; set; }
+        public string Gender { get; set; }
+        public string Director { get; set; }
+        public int Duraction { get; set; }
+    }
+}
